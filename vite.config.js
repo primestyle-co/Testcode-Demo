6ffc7f6ce77cb8966b113ea292e818a78562f294
AnhTung <anh.tung@primelabo.com.vn>
2022-07-26 11:49:00 +0700

    unit test front end

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+const path = require('path');
  
         // laravel(['resources/js/app.js']),
 export default defineConfig({
+    resolve:{
+        alias:{
+            '@' : path.resolve(__dirname, './resources/js')
+        }
+    },
     plugins: [
         laravel(['resources/js/app.js']),
         vue({
